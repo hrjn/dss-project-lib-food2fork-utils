575497312a79f5d24574cae5cfef44e7f429b283
Harizo Rajaona <harizo.rajaona@gmail.com>
2019-02-22 14:53:11 +0100

    Added missing utils from dl plugin

--- python_utils.py
+++ python_utils.py
@@ -1,3 +1,8 @@
+
+def get_file_path(folder_path, file_name):
+    # Be careful to enforce that folder_path and file_name are actually strings
+    return os.path.join(safe_str(folder_path), safe_str(file_name))
+
 def download_files_to_managed_folder(folder_path, files_info, chunk_size=8192):
     total_size = 0
     bytes_so_far = 0
@@ -7,7 +12,7 @@ def download_files_to_managed_folder(folder_path, files_info, chunk_size=8192):
         file_info["response"] = response
     update_time = time.time()
     for file_info in files_info:
-        with open(utils.get_file_path(folder_path, file_info["filename"]), "wb") as f:
+        with open(get_file_path(folder_path, file_info["filename"]), "wb") as f:
             for content in file_info["response"].iter_content(chunk_size=chunk_size):
                 bytes_so_far += len(content)
                 # Only scale to 80% because needs to compute model summary after download
